3e460ad2b771d6092c202b53fdb92bcc5e91ec1b
Caio <caio-pimentel@hotmail.com>
2025-07-09 10:52:13 -0300

    Initial commit

---
+++ src/main/java/com/dev/caiofpimentel/portifoliocaio/PortifoliocaioApplication.java
@@ -0,0 +1,13 @@
+package com.dev.caiofpimentel.portifoliocaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifoliocaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifoliocaioApplication.class, args);
+	}
+
+}
