11bad9f5e7756c8fe1801f89851828cb367d4bc9
Caio <caio-pimentel@hotmail.com>
2025-07-09 18:20:27 -0300

    feat: adiciona o controller, o controllerUi e pagina index para servir de pagina inicial

--- src/main/java/com/dev/caiofpimentel/portifoliocaio/controller/Controller.java
+++ src/main/java/com/dev/caiofpimentel/portifoliocaio/controller/Controller.java
@@ -1,4 +1,11 @@
 package com.dev.caiofpimentel.portifoliocaio.controller;
 
-public class controller {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
 }
